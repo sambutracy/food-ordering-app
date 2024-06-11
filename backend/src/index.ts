a8a806e1cceecc19b7ccd546999442de1fd76e6f
sambutracy <tracysambut@gmail.com>
2024-06-11 18:28:29 +0300

    Add MainNav and log in functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response} from "express";
-import cors from"cors";
+import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
   return(
@@ -10,9 +12,15 @@ const Header = () => {
             >
               Speedy Eats Delivery  
             </Link>
+            <div className="md:hidden">
+                <MobileNav />
+            </div>
+            <div className="hidden md:block">
+                <MainNav />
+            </div>
         </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
