15016353785e11b950b192c35e33b531378a6134
sambutracy <tracysambut@gmail.com>
2024-07-01 00:18:24 +0300

    updates

---
+++ frontend/src/api/MyUserApi.tsx
@@ -0,0 +1,41 @@
+import { User } from "@/types";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useMutation, useQuery } from "react-query";
+//import { toast } from "sonner";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+type CreateUserRequest = {
+    auth0Id: string;
+    email: string;
+};
+
+export const useCreateMyUser = () => {
+    const { getAccessTokensSilently } = useAuth0();
+    const createMyUserRequest = async (user: CreateUserRequest) => {
+        const accessToken = await getAccessTokensSilently();
+        const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),  
+        });
+
+        if (!response.ok){
+            throw new Error("Failed to create user");
+        }
+    };
+
+    const { 
+        mutateAsync: createUser,
+        isLoading,
+        isError,
+        isSuccess,
+    } = useMutation(createMyUserRequest);
+
+    return {
+        createUser,
+    }
+};
