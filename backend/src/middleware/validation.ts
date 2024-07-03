959ea01026cd744612ae8de83552de59f0806f68
sambutracy <tracysambut@gmail.com>
2024-07-03 20:07:34 +0300

    Resolved merge conflicts

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -1,15 +1,25 @@
+<<<<<<< Updated upstream
 import { body, validationResult } from "express-validator";
 import { Request, Response, NextFunction } from "express";
 
 const handleValidationErrors = async (
   req: Request,
   res: Response,
+=======
+import { Request, Response, NextFunction } from "express";
+import { body, validationResult } from "express-validator";
+
+const handleValidationErrors = async (
+  req: Request, 
+  res: Response, 
+>>>>>>> Stashed changes
   next: NextFunction
 ) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+<<<<<<< Updated upstream
   next();
 };
 
@@ -45,5 +55,15 @@ export const validateMyRestaurantRequest = [
   body("menuItems.*.price")
     .isFloat({ min: 0 })
     .withMessage("Menu item price is required and must be a postive number"),
+=======
+  next();  
+};
+
+export const validateMyUsertRequest = [
+  body("name").isString().notEmpty().withMessage("Name must be a string"),
+  body("addressLine1").isString().notEmpty().withMessage("address must be a string"),
+  body("city").isString().notEmpty().withMessage("city must be a string"),
+  body("country").isString().notEmpty().withMessage("country must be a string"),
+>>>>>>> Stashed changes
   handleValidationErrors,
 ];
