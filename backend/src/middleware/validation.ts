f0d429f30893d650c332cfe1e6beacf961d18af8
sambutracy <tracysambut@gmail.com>
2024-07-04 00:12:47 +0300

    Resolved conflicts and applied stashed changes

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -1,36 +1,23 @@
-<<<<<<< Updated upstream
 import { body, validationResult } from "express-validator";
 import { Request, Response, NextFunction } from "express";
 
 const handleValidationErrors = async (
   req: Request,
   res: Response,
-=======
-import { Request, Response, NextFunction } from "express";
-import { body, validationResult } from "express-validator";
-
-const handleValidationErrors = async (
-  req: Request, 
-  res: Response, 
->>>>>>> Stashed changes
   next: NextFunction
 ) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-<<<<<<< Updated upstream
-  next();
+  next();  
 };
 
-export const validateMyUserRequest = [
+export const validateMyUsertRequest = [
   body("name").isString().notEmpty().withMessage("Name must be a string"),
-  body("addressLine1")
-    .isString()
-    .notEmpty()
-    .withMessage("AddressLine1 must be a string"),
-  body("city").isString().notEmpty().withMessage("City must be a string"),
-  body("country").isString().notEmpty().withMessage("Country must be a string"),
+  body("addressLine1").isString().notEmpty().withMessage("address must be a string"),
+  body("city").isString().notEmpty().withMessage("city must be a string"),
+  body("country").isString().notEmpty().withMessage("country must be a string"),
   handleValidationErrors,
 ];
 
@@ -55,15 +42,5 @@ export const validateMyRestaurantRequest = [
   body("menuItems.*.price")
     .isFloat({ min: 0 })
     .withMessage("Menu item price is required and must be a postive number"),
-=======
-  next();  
-};
-
-export const validateMyUsertRequest = [
-  body("name").isString().notEmpty().withMessage("Name must be a string"),
-  body("addressLine1").isString().notEmpty().withMessage("address must be a string"),
-  body("city").isString().notEmpty().withMessage("city must be a string"),
-  body("country").isString().notEmpty().withMessage("country must be a string"),
->>>>>>> Stashed changes
   handleValidationErrors,
 ];
