54b2c60b4c7eea694683bebe2946ae41d249e42f
vintage254 <derricknjuguna414@gmail.com>
2024-07-03 09:27:46 +0300

    backend updates

---
+++ backend/src/controllers/MyRestaurantController.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import Restaurant from "../models/restaurant";
+import cloudinary from "cloudinary";
+import mongoose from "mongoose";
+import Order from "../models/order";
+
+const getMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.userId });
+    if (!restaurant) {
+      return res.status(404).json({ message: "restaurant not found" });
+    }
+    res.json(restaurant);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Error fetching restaurant" });
+  }
+};
+
+const createMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    const existingRestaurant = await Restaurant.findOne({ user: req.userId });
+
+    if (existingRestaurant) {
+      return res
+        .status(409)
+        .json({ message: "User restaurant already exists" });
+    }
+
+    const image = req.file as Express.Multer.File;
+    const base64Image = Buffer.from(image.buffer).toString("base64");
+    const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+
+
+    const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
+
+    const restaurant = new Restaurant(req.body);
+    restaurant.imageUrl = uploadResponse.url;
+    restaurant.user = new mongoose.Types.ObjectId(req.userId);
+    restaurant.lastUpdated = new Date();
+    await restaurant.save();
+
+    res.status(201).send(restaurant);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import MyUserRoute from "./routes/MyUserRoute";
+import { env } from "process";
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(()=> console.log("Connected to Database!"));
@@ -11,7 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
 app.use("/api/my/user", MyUserRoute);
+
 
 app.listen(7000, ()=> {
     console.log("server staterted on localhost:7000");
