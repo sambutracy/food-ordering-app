3948726029bb7eadf40432bf74ead8436684f2e6
sambutracy <tracysambut@gmail.com>
2024-07-04 16:36:11 +0300

    fix indent error on AuthCallbackPage

--- frontend/src/pages/AuthCallbackPage.tsx
+++ frontend/src/pages/AuthCallbackPage.tsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
-  const AuthCallbackPage = () => {
+const AuthCallbackPage = () => {
   const navigate = useNavigate();
   const { user } = useAuth0();
   const { createUser } = useCreateMyUser();
@@ -11,10 +11,10 @@ import { useNavigate } from "react-router-dom";
 
   useEffect(() => {
     if (user?.sub && user.email && !hasCreatedUser.current) {
-        createUser({ auth0Id: user.sub, email: user.email });
-        hasCreatedUser.current = true;
-      }
-      navigate("/");
+      createUser({ auth0Id: user.sub, email: user.email });
+      hasCreatedUser.current = true;
+    }
+    navigate("/");
   }, [createUser, navigate, user]);
 
   return <>Loading...</>;
